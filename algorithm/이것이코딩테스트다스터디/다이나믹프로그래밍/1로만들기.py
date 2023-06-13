b5b6870a32882d050b1f53fe70cbda4278d8bef2
KwangHyeonKim <rhkdguskim@naver.com>
2023-06-14 01:58:06 +0900

    다이나믹 프로그래밍

---
+++ algorithm/이것이코딩테스트다스터디/다이나믹프로그래밍/1로만들기.py
@@ -0,0 +1,18 @@
+X = int(input())
+counter = 0
+dp = [0] * 30001
+dp[0] = 1
+i = 0
+while(dp[i] != X):
+    if(X >= max(dp[i]*5, dp[i]*3, dp[i]*2, dp[i] + 1)):
+        dp[i+1] = max(dp[i]*5, dp[i]*3, dp[i]*2, dp[i] + 1)
+    elif (X >= max(dp[i]*3, dp[i]*2, dp[i] + 1)):
+        dp[i+1] = max(dp[i]*3, dp[i]*2, dp[i] + 1)
+    elif (X >= max(dp[i]*2, dp[i] + 1)):
+        dp[i+1] = max(dp[i]*2, dp[i] + 1)
+    else :
+        dp[i+1] = dp[i] + 1
+
+    i += 1
+    
+print(i)
