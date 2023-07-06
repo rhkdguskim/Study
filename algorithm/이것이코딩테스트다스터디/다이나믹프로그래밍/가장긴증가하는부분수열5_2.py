78fd29194c30124e19b8ba46e6ec2f12a9bc7d94
KwangHyeonKim <rhkdguskim@naver.com>
2023-07-06 23:56:00 +0900

    동적계획법

---
+++ algorithm/이것이코딩테스트다스터디/다이나믹프로그래밍/가장긴증가하는부분수열5_2.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/14003
+import sys
+
+input = sys.stdin.readline
+
+import bisect
+n = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+
+ans=[0]*n
+lis = [-1000000001]
+for i in range(n):
+    if arr[i]> lis[-1]:
+        lis.append(arr[i])
+        ans[i]=len(lis)
+    else:
+        temp = bisect.bisect_left(lis, arr[i])
+        lis[temp] = arr[i]
+        ans[i] = temp + 1
+temp = [0] * len(lis)
+cnt = len(lis)
+for i in range(len(arr) - 1, -1, -1):
+    if ans[i] == cnt:
+        cnt-=1
+        temp[cnt] = arr[i]
+l = len(lis)
+print(l-1)
+print(*temp[1:])
