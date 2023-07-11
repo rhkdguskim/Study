06e520ba30579360d3bb2d268844bfa59df90ffc
KwangHyeonKim <rhkdguskim@naver.com>
2023-07-11 23:30:46 +0900

    다이나믹 프로그래밍 스터디

--- algorithm/이것이코딩테스트다스터디/다이나믹프로그래밍/쉬운계단수.py
+++ algorithm/이것이코딩테스트다스터디/다이나믹프로그래밍/쉬운계단수.py
@@ -1,13 +1,18 @@
 # https://www.acmicpc.net/problem/10844
 N = int(input())
-dp = [0 for _ in range(N+1)]
-dp[1] = 9
-dp[2] = 17
+dp = [[0 for _ in range(11)] for _ in range(N+1)]
 
-for i in range(3, N+1):
-    dp[i] = 2*(dp[i-1]) -1
+for i in range(1, len(dp[0]) -1):
+    dp[1][i] = 1
+    
+for i in range(2, len(dp)):
+    for j in range(0, len(dp[i]) -1):
+        if j == 9:
+            dp[i][j] = dp[i-1][j-1]
+        elif j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-if N == 1:
-    print(1)
-else:
-    print(dp[N] % 1000000000)
+print(dp[N])
+print(sum(dp[N]) % 1000000000)
