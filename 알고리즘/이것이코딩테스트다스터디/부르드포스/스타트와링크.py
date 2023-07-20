548e376b8df42a58f0e8e0b5caa5ba779287ae24
KwangHyeonKim <rhkdguskim@naver.com>
2023-07-20 23:54:35 +0900

    백트래킹 브루트포스 알고리즘

--- 알고리즘/이것이코딩테스트다스터디/부르드포스/스타트와링크.py
+++ 알고리즘/이것이코딩테스트다스터디/부르드포스/스타트와링크.py
@@ -1,34 +1,51 @@
 # https://www.acmicpc.net/problem/14889
-# 1. 선수들의 조합을 만든다.
+# 1. 선수들의 조합을 만든다. ( 재귀로 만든다. )
 # 2. 팀의 능력치를 계산하고, 차이점을 계산하여 최소값에 업데이트한다.
-# 3. 다음 팀의 조합을 계산한다. 계산할때 최소값보다 커진다면 백트래킹 한다.
-from itertools import combinations
+import itertools
 N = int(input())
 
 playerscore = []
 for _ in range(N):
-    playerscore.append(map(int, input().split()))
+    playerscore.append(list(map(int, input().split())))
 
-team = [i for i in range(N)]
-teams = list(combinations(team, len(team)//2))
+players = [i for i in range(N)]
 
-print(teams)
 team1 = []
-team2 = []
-for player1 in teams:
-    team1.append(player1)
-    team2.append(tuple(player2 for player2 in team if player2 not in player1))
-    
-mindata = int(10e9)
-
-print(team1)
-print(team2)
-# def dfs(team1, team2):
-    
-#     score1 = list(combinations(team1, 2))
-#     score2 = list(combinations(team1, 2))
-    
-#     sum = 0
-#     for i in range(len(score1)):
-#         score1[i][0]score1[i][1]
+minvalue = int(10e9)
+def dfs(playerscore):
+    global minvalue
+    if len(team1) == N // 2: # 종료조건
+        score1 = 0
+        score2 = 0
+        team2 = []
+        for player2 in players: # team2의 플레이어를 구한다
+            if player2 not in team1:
+                team2.append(player2)
+        newteam1 = list(itertools.combinations(team1, 2))
+        newteam2 = list(itertools.combinations(team2, 2))
         
+        for a,b in newteam1:
+            score1 += playerscore[a][b]
+            score1 += playerscore[b][a]
+        
+        for a,b in newteam2:
+            score2 += playerscore[a][b]
+            score2 += playerscore[b][a]
+            
+        minvalue = min(minvalue, abs(score1 - score2))
+        print(minvalue, newteam1, newteam2)
+        return minvalue
+
+
+    for i in range(N):
+        if i not in team1: # 팀에 자기자신을 넣지 않는다.
+            team1.append(i)
+            dfs(playerscore)
+            team1.pop()
+            
+
+team1.append(0)
+dfs(playerscore)
+team1.pop()
+
+print(minvalue)
