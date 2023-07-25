8cde11319866e7dec85e31618ce5290980967595
김광현 <kwanghyeon.kim@mirero.co.kr>
2023-07-25 13:04:19 +0900

    데이크스트라 점심 스터디

--- 알고리즘/이것이코딩테스트다스터디/다익스트라/특정한최단경로.py
+++ 알고리즘/이것이코딩테스트다스터디/다익스트라/특정한최단경로.py
@@ -1,4 +1,6 @@
 # https://www.acmicpc.net/problem/1504
+# 1) 1-> V1에서 v2최소거리 -> 나머지노드
+# 2) 1-> v2에서 v1최소거리 -> 나머지노드
 import heapq
 INF = int(10e9)
 N, M = map(int, input().split()) # N 정점의개수, M 간선의 개수
@@ -13,6 +15,7 @@ v1, v2 = map(int, input().split())
 
 distance = [INF] * (N+1)
 distance2 = [INF] * (N+1)
+distance3 = [INF] * (N+1)
 def dijkstra(start, distance):
     queue = []
     distance[start] = 0
@@ -27,8 +30,19 @@ def dijkstra(start, distance):
             if distance[i[0]] > cost:
                 distance[i[0]] = cost
                 heapq.heappush(queue, [distance[i[0]], i[0]])
-                
-dijkstra(v1, distance)
-print(distance)
-dijkstra(v2, distance2)
-print(distance2)
+
+
+dijkstra(v1, distance) # V1에서 나머지노드까지 최소거리
+dijkstra(v2, distance2) # v2에서 나머지노드까지 최소거리
+dijkstra(1, distance3) # 1에서 나머지노드까지 최소거리
+
+minvalue = INF
+
+for i in range(N):
+    if i != 1:
+        minvalue = min(minvalue, distance3[v1] + distance[v2] + distance2[i], distance3[v1] + distance2[v2] + distance[i])
+    
+if minvalue == INF:
+    print(-1)
+else:
+    print(minvalue)
