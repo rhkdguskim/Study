52ee6b0e99c3695ff4abd8e1c721fbb749f4085c
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-07-26 17:36:54 +0900

    데이터 베이스 스터디

--- 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/외원판순회2.py
+++ 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/외원판순회2.py
@@ -1,41 +1,28 @@
-from collections import deque
 N = int(input())
 graph = []
 for _ in range(N):
     graph.append(list(map(int ,input().split())))
 
-minvalue = int(10e9)
-
-def dfs(start, nodelist, sum):
-    global minvalue
-    if sum > minvalue:
-        return int(10e9)
-    
-    if len(nodelist) == N:
-        #print(nodelist, sum)
-        if graph[start][nodelist[0]]:
-            minvalue = min(minvalue, sum + graph[start][nodelist[0]]) # 마지막은 자기자신으로 방문한다. 마지막도착지점에서 자기자신으로 갈 수 없을 경우
-            return minvalue
+INF = int(1e9)
+dp = [[None] * (1 << N) for _ in range(N)] # N을 좌 shfit 하면 N*2 가 된다.
+def dfs(i, visited):
+    if visited == (1 << N) -1: # 모든 도시를 다 방문 했다면 (비트 의 전체합 -1 이면 모두 방문한 상황)
+        if graph[i][0]:
+            return graph[i][0] # 출발점으로 가는 경로가 있다면 출발점으로 가는 경로 출력
         else:
-            return int(10e9)
+            return INF # 출발점으로 가는 경로가 없다면 INF 출력
+        
+    if dp[i][visited] != None: # 이미 최소비용이 계산되어있다면 출력
+        return dp[i][visited]
     
-    for x in range(N):
-        if x not in nodelist:
-            if graph[start][x] == 0: # 갈수없는 경우 순회중단
-                return int(10e9)
-            sum += graph[start][x]
-            nodelist.append(x)
-            dfs(x,nodelist, sum)
-            nodelist.pop()
+    temp = INF
+    for x in range(1, N):
+        if not graph[i][x] or visited & (1 << x): # 가는 경로가 없거나 이미 방문한 도시라면 skip
+            continue
+        
+        temp = min(temp, dfs(x, visited | (1 << x)) + graph[i][x]) # i가 4일경우 10000 값을 or 연산한다.
+        
+    dp[i][visited] = temp
+    return temp
             
-    return minvalue
-
-minvalue = int(10e9)
-nodelist = deque()
-for i in range(N):
-    nodelist.append(i)
-    minvalue = min(dfs(i,nodelist, 0), minvalue)
-    end = nodelist.pop()
-        
-        
-print(minvalue)
+print(dfs(0,1))
