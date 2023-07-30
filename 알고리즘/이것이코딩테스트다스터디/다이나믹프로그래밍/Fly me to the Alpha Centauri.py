c6a90594fe7801ec8e7b0962dd8fb0b9ea139d16
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-07-30 23:58:08 +0900

    알고리즘 스터디

--- 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/Fly me to the Alpha Centauri.py
+++ 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/Fly me to the Alpha Centauri.py
@@ -1,29 +1,20 @@
 # https://www.acmicpc.net/problem/1011
-from collections import deque
 T = int(input())
+
+def distance(cost):
+    count = 0  # 이동 횟수
+    move = 1  # count별 빈도수
+    move_plus = 0  # 이동한 거리의 합
+    while move_plus < cost :
+        count += 1
+        move_plus += move  # count 수에 해당하는 move를 더함
+        if count % 2 == 0 :  # count가 2의 배수일 때, 
+            move += 1  
+    
+    return count
+    
+    
 
 for _ in range(T):
     x, y = map(int, input().split())
-    dp = [y+1 for _ in range(y+1 - x)]
-    queue = deque()
-    queue.append([1, 1])
-    dp[0] = 0
-    dp[1] = 1
-    while queue:
-        distance, year = queue.popleft()
-        if year > 0 and y+1 - x > distance + year + 1:
-            cost = dp[distance] + 1
-            
-            if year - 1 > 0 and dp[distance + year-1] > cost: # K -1
-                dp[distance + year-1] = cost 
-                queue.append([distance + year-1, year-1])
-            if dp[distance + year] > cost: # K
-                dp[distance + year] = cost
-                queue.append([distance + year, year])
-            if dp[distance + year+1] > cost: # K+1
-                dp[distance + year+1] = cost
-                queue.append([distance + year+1, year+1])
-    if y-x == 1:
-        print(1)
-    else:
-        print(dp[y-1 - x]+1)
+    print(distance(y-x))
