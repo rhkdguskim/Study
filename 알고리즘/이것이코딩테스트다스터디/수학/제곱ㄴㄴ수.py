ac0dd93fa70ec20411375fc2f19deab3b11a3373
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-08-04 23:08:49 +0900

    제곱수 문제

--- 알고리즘/이것이코딩테스트다스터디/수학/제곱ㄴㄴ수.py
+++ 알고리즘/이것이코딩테스트다스터디/수학/제곱ㄴㄴ수.py
@@ -1,12 +1,19 @@
 # https://www.acmicpc.net/problem/1016
-import math
 min, max = map(int, input().split())
 visited = [1] * (max - min + 1) # 최대 1,000,000
 
-def isValied(number):
-    tryn = math.sqrt(number)
-    temp = 2
-    for _ in range(tryn):
-        pass
+i = 2
+while i*i <= max:
+    temp = min//(i*i) # i*i 으로 나눌수 있는 최소값을 찾는다.
+    if min%(i*i) != 0: # 만약 소숫점이 생긴다면 1을 추가해준다.
+        temp += 1
 
-print(sum(visited))
+    while temp *i*i <= max: # 에라토스테네스의 체 처럼 나누어지는수를 초기화 해준다.
+        print(temp * i*i - min, i, temp)
+        if visited[temp * i*i - min]:
+            visited[temp * i*i - min] = 0
+                
+        temp += 1
+    i += 1
+
+print(visited.count(1))
