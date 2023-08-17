f35ecd3ee86d4fb5d34b991c64c096e6bb7b63d9
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-08-17 22:49:05 +0900

    감소하는수 문제풀이

--- 알고리즘/이것이코딩테스트다스터디/부르드포스/감소하는 수.py
+++ 알고리즘/이것이코딩테스트다스터디/부르드포스/감소하는 수.py
@@ -1,22 +1,24 @@
 # https://www.acmicpc.net/problem/1038
 # N번째 감소하는 수를 출력
-
 N = int(input())
 
 decrease_num = [[] for _ in range(10)]
 for i in range(10):
     decrease_num[i].append(i)
 
-for i in range(1,10):
-    new_decrease_num = [[] for _ in range(10)]
-    new_decrease_num[0].append(0)
-    for j in range(10):
-        for num in decrease_num[j]:
-            print(num)
-        if i > j:
-            new_decrease_num[i].append(list(num for num in decrease_num[j]))
+result = [0,1,2,3,4,5,6,7,8,9]
+for i in range(1, 10): # 자리수 반복 ( 두번째 자리수 부터 시작)
+    temp_list = [[] for _ in range(10)]
+    for j in range(1, 10):
+        for k in range(j):
+            for num in decrease_num[k]:
+                temp_list[j].append(int(str(j) + str(num)))
+                result.append(int(str(j) + str(num)))
     
-    print(new_decrease_num)
-    decrease_num = new_decrease_num
-        
+    decrease_num = temp_list
+
+if len(result) > N:
+    print(result[N])
+else:
+    print(-1)
         
