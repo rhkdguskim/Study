77e006d911f198497f465a0cd5074210b7fcb243
김광현 <kwanghyeon.kim@mirero.co.kr>
2023-08-21 12:54:17 +0900

    알고리즘 스터디

--- 알고리즘/이것이코딩테스트다스터디/탐색/텀프로젝트.py
+++ 알고리즘/이것이코딩테스트다스터디/탐색/텀프로젝트.py
@@ -4,23 +4,27 @@ sys.setrecursionlimit(100001)
 T = int(input())
 
 
-def dfs(student, root):
-    if student == graph[student]: # 자기자신을 뽑는경우
-        dp[student] = 0
-        return dp[student]
+def dfs(student):
+    global result
+    visited[student] = True
+    cycle.append(student)
+    newstudent = graph[student]
     
-    if graph[student] == root: # 사이클이 한번 돌은경우 (이들은 서로 그룹이 된다.)
-        dp[student] = 1
-        return dp[student]
-    
-    dp[student] = dfs(graph[student], root)
-    return dp[student]
+    if visited[newstudent]:
+        if newstudent in cycle:
+            result += cycle[cycle.index(newstudent):] # 자기자신으로부터 마지막팀원까지 그룹핑
+        return
+    else:
+        dfs(newstudent)
 
 for _ in range(T):
     n = int(input())
-    graph = [0] + list(map(int, input().split())) # 0을 추가하는이유는 0번인 학생이 없기 때문
-    dp = [None for _ in range(n+1)]
+    graph = [0] + list(map(int, input().split()))
+    result = []
+    visited = [False for _ in range(n+1)]
     for i in range(1, n+1):
-        if dp[i] is None:
-            dp[i] = dfs(i, i)
-    print(dp.count(1))
+        if not visited[i]:
+            cycle = []
+            dfs(i)    
+            
+    print(n - len(result))
