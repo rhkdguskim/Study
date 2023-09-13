f696367244916e9c7a72e954cf38269c908f5f76
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-09-13 23:57:11 +0900

    알고리즘 스터디

--- 알고리즘/이것이코딩테스트다스터디/다국어/DualPriorityQueue.py
+++ 알고리즘/이것이코딩테스트다스터디/다국어/DualPriorityQueue.py
@@ -8,60 +8,45 @@
 # 우선순위큐 (?)이분탐색으로 가능할듯?
 
 # 테스트케이스T, K는 명령어수(1000000), 그다음에는 명령어가 주어진다.
-from collections import deque
-
+import heapq
 
 T = int(input())
 for _ in range(T):
     k = int(input())
-    queue = deque()
-    minqueue = deque()
-    maxqueue = deque()
-    
-    for _ in range(k):
-        cmd , num = map(str, input().split())
-        number = int(num)
-        if cmd == 'I': # 큐에 삽입
-            if minqueue and minqueue[0] >= number:
-                minqueue.append(number)
-            elif maxqueue and number >= maxqueue[0]:
-                maxqueue.appendleft(number)
-            else:
-                if not minqueue:
-                    minqueue.append(number)
-                elif not maxqueue:
-                    maxqueue.append(number)
-                else:
-                    if number > minqueue[0]:
-                        while number > maxqueue[-1]:
-                            movenum = maxqueue.pop()
-                            minqueue.appendleft(movenum)
-                        
-                        maxqueue.append(number)
-                    else:
-                        minqueue.append(number)
+    minq = []
+    maxq = []
+    visited = [False] * k
+    for i in range(k):
+        op , temp = map(str, input().split())
+        number = int(temp)
+        if op == "I":
+            heapq.heappush(minq, [number, i])
+            heapq.heappush(maxq, [-number, i])
+            visited[i] = True
+        else:
+            if number == 1: # 큰값을 제거하라
+                while maxq and not visited[maxq[0][1]]:
+                    heapq.heappop(maxq)
                     
-        else:
-            if number == 1: # 우선순위가 높은 숫자를 삭제
-                if maxqueue:
-                    maxqueue.popleft()
-                elif minqueue:
-                    minqueue.popleft()
+                if maxq:
+                    visited[maxq[0][1]] = False
+                    heapq.heappop(maxq)
             else:
-                if minqueue: # 우선순위가 낮은 숫자를 삭제
-                    minqueue.pop()
-                elif maxqueue:
-                    maxqueue.pop()
+                while minq and not visited[minq[0][1]]:
+                    heapq.heappop(minq)
                     
-    if minqueue or maxqueue:
-        if maxqueue:
-            print(maxqueue[0], end=' ')
-        else:
-            print(minqueue[0], end=' ')
-            
-        if minqueue:
-            print(minqueue[-1], end =' ')
-        else:
-            print(maxqueue[-1], end=' ')
+                if minq:
+                    visited[minq[0][1]] = False
+                    heapq.heappop(minq)
+                    
+    while minq and not visited[minq[0][1]]:
+        heapq.heappop(minq)
+        
+    while maxq and not visited[maxq[0][1]]:
+        heapq.heappop(maxq)
+        
+    if minq and maxq:
+        print(-maxq[0][0], minq[0][0])
     else:
         print("EMPTY")
+        
--- 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/합분해.py
+++ 알고리즘/이것이코딩테스트다스터디/다이나믹프로그래밍/합분해.py
@@ -1,1 +1,14 @@
 #https://www.acmicpc.net/problem/2225
+N , K = map(int, input().split())
+
+dp = [[1 for _ in range(N+1) ] for _ in range(K)]
+
+for i in range(1, K):
+    for j in range(len(dp[i])):
+        dp[i][j] = 0
+        for k in range(j+1):
+            dp[i][j] += dp[i-1][k]
+            
+        dp[i][j] = dp[i][j] % 1000000000
+            
+print(dp[K-1][N] % 1000000000)
