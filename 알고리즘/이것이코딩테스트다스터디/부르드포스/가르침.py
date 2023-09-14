965b4e5176a7272683793ecbcd29e683b14acf8c
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-09-14 22:01:49 +0900

    가르침 문제 풀이

--- 알고리즘/이것이코딩테스트다스터디/부르드포스/가르침.py
+++ 알고리즘/이것이코딩테스트다스터디/부르드포스/가르침.py
@@ -2,26 +2,49 @@
 from itertools import combinations
 
 N, K = map(int, input().split())
-words = set(('a','n','t','i','c'))
-alpabat = set(('b','d','e','f','g','h','j','k','l','m','o','p','q','r','s','u','v','w','x','y','z'))
+northword = list(['a','n','t','i','c'])
+alpabat = list(['b','d','e','f','g','h','j','k','l','m','o','p','q','r','s','u','v','w','x','y','z'])
 
-charlist = list()
+words = list()
 
 for _ in range(N):
-    char = set(input())
-    if set(words).issubset(char):
-        charlist.append(char)
+    word = list(input())
+    words.append(word)
     
+
+
+learned = [False for _ in range(26)]
+
+for alpa in northword:
+    learned[ord(alpa)-ord('a')] = True
+
+
 maxcounter = 0
-visited = dict()
-if K-5 > 0:
-    for alpa in combinations(alpabat, K-5):
-        counter = 0
-        for char in charlist:
-            newlist = alpa + ('a','n','t','i','c')
-            if set(char).issubset(set(newlist)):
-                counter += 1
-                
-        maxcounter = max(maxcounter,counter)
-    
-print(maxcounter)
+
+if K-4 > 0:
+    for alpalist in combinations(alpabat, K-5):
+        count = 0
+        
+        for alpa in alpalist:
+            learned[ord(alpa)-ord('a')] = True
+            
+        # 계산하는 로직과 max값 리뉴얼
+        for word in words:
+            canRead = True
+            for temp in word:
+                if not learned[ord(temp)-ord('a')]: # 배우지 않는 알파벳이 있다면
+                    canRead = False
+                    break
+            
+            if canRead:
+                count += 1
+                    
+        if count > maxcounter:
+            maxcounter = count
+            
+        for alpa in alpalist:
+            learned[ord(alpa)-ord('a')] = False
+            
+    print(maxcounter)
+else:
+    print(0)
