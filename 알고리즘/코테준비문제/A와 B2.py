3cae73f0566ab094fdeef3fecef56a2e22c3ae50
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-09-18 22:47:23 +0900

    알고리즘 스터디

--- 알고리즘/코테준비문제/A와 B2.py
+++ 알고리즘/코테준비문제/A와 B2.py
@@ -1,23 +1,35 @@
 #https://www.acmicpc.net/problem/12919
-from copy import deepcopy
 S = str(input())
 T = str(input())
 
-
 findFlag = False
+def charReverse(char):
+    newchar = ""
+    
+    length = len(char) // 2
+    arr = list(char)
+    for i in range(length):
+        arr[i], arr[len(arr)-1 - i] = arr[len(arr)-1 - i], arr[i]
+        
+    for c in arr:
+        newchar += c
+        
+    return newchar
+
+
 def AandB(char):
     global findFlag
     if len(char) == len(T):
-        print(char, T)
         if char == T:
             findFlag = True
         return
+                
             
     newchar1 = char + "A"
     newchar2 = char + "B"
     
     AandB(newchar1)
-    AandB(newchar2)
+    AandB(charReverse(newchar2))
             
 AandB(S)            
             
