fcd8f1a12e43807f05e54e6eb3e208b9acb04ca5
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-09-22 23:53:36 +0900

    알고리즘 문제풀이

--- 알고리즘/코테준비문제/가운데를말해요.py
+++ 알고리즘/코테준비문제/가운데를말해요.py
@@ -1,66 +1,24 @@
 # https://www.acmicpc.net/problem/1655
 # 큐를 2개로 문제를 해결한다.
 # 왼쪽 오른쪽 한번씩 번갈아가면서 큐를 추가한다.
-from collections import deque
+import heapq
 import sys
-leftq = deque()
-rightq = deque()
-initq = []
+leftq = []
+rightq = []
 N = int(input())
-counter = 0
 
 for _ in range(N):
-    number = int(sys.stdin.readline())
-    counter += 1
-    if counter == 1:
-        leftq.append(number)
-        rightq.append(number)
-    elif counter == 2:
-        if number > rightq[0]:
-            rightq[0] = number
-        else:
-            rightq.pop()
-            if leftq[-1] > number:
-                leftq.appendleft(number)
-            else:
-                leftq.append(number)
-    else:
-        if leftq[-1] > number:
-            leftq.appendleft(number)
-        else:
-            if rightq[0] > number:
-                rightq.appendleft(number)
-            else:
-                rightq.append(number)
+    num = int(sys.stdin.readline())
     
-    
-    if counter > 2:
-        if counter % 2 == 0:
-            while (len(leftq) != len(rightq)): # 짝수일때
-                if len(leftq) > len(rightq):
-                    newnum = leftq.pop()
-                    if rightq[0] >= number:
-                        rightq.appendleft(newnum)
-                    else:
-                        rightq.append(newnum)
-                else:
-                    newnum = rightq.popleft()
-                    if leftq[-1] > newnum:
-                        leftq.appendleft(newnum)
-                    else:
-                        leftq.append(newnum)
-            
-            print(min(leftq[-1], rightq[0]))
-        else:
-            while (abs(len(leftq) - len(rightq)) != 1): # 홀수일때
-                if len(leftq) > len(rightq):
-                    rightq.appendleft(leftq.pop())
-                else:
-                    leftq.appendleft(rightq.popleft())
-                    
-            if len(leftq) > len(rightq):
-                print(leftq[-1])
-            else:
-                print(rightq[0])
+    if len(leftq) != len(rightq):
+        heapq.heappush(rightq, num)
     else:
-        print(min(leftq+rightq))
+        heapq.heappush(leftq, -num)
+    
+    if rightq and rightq[0] < -leftq[0]:
+        rightnum = heapq.heappop(rightq)
+        leftnum = heapq.heappop(leftq)
+        heapq.heappush(rightq, -leftnum)
+        heapq.heappush(leftq, -rightnum)
+    
+    print(-leftq[0])
