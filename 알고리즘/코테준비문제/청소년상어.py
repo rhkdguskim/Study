2c98a17ece084a7ef617f036a68d8a2c4608e4c8
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-10-12 23:29:14 +0900

    알고리즘 스터디

--- 알고리즘/코테준비문제/청소년상어.py
+++ 알고리즘/코테준비문제/청소년상어.py
@@ -1,131 +1,93 @@
 # https://www.acmicpc.net/problem/19236
 from pprint import pprint
-import heapq
-
 # 1. 상어가 0,0으로 이동한다. 물고기를 먹어버린다. 물고기 방향으로 바뀐다.
 # 2. 물고기가 작은 순서부터 이동한다.
 # 3.
-
 from copy import deepcopy
+
+EMPTY = -1
+SHARK = 20
 
 N = 4 # 4x4
 graph = [[[] for _ in range(N)] for _ in range(N)]
-direction = [(0, 0), (-1, 0), (-1, -1), (0, -1), (1, -1), (-1, 0), (1, 1), (0, 1), (-1, 1)] # 자기자신, ↑, ↖, ←, ↙, ↓, ↘, →, ↗ 를 의미한다.
-
-fishqueue = [] # 물고기를 순서대로 움직일 큐
+direction = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)] # ↑, ↖, ←, ↙, ↓, ↘, →, ↗ 를 의미한다.
 
 for i in range(N):
     temp = list(map(int, input().split()))
     j = 0
     for j in range(0,len(temp), 2):
-        graph[i][j//2] = [temp[j], temp[j+1]] # 물고기번호, 물고기방향
-        fishqueue.append((temp[j], temp[j+1], i , j//2)) # 물고기 번호와 방향, 좌표를 갖는 큐를 만든다.
+        graph[i][j//2] = [temp[j], temp[j+1]-1] # 물고기번호, 물고기방향
 
-# 1. 상어가 공간에 들어가 물고기를 먹는다.
-
-
-# 시계반대방향 45도로 도는경우
-def rotate(type):
-    if type == len(direction)-1: # 마지막인경우
-        return 1
-    else:
-        return type + 1
-
-def canmoveshark():
-    global ans
-    if graph[0][0][0] == 0: # 0은 빈칸으로 표현된다.
-        return None
-
-    ans += graph[0][0][0] # 먹어버린 가중치 추가
-    graph[0][0][0] = 20 # 물고기를 먹어버린뒤, 상어의 위치를 표기한다.
-    return graph[0][0][1] # 상어의 방향을 나타낸다.
-
-# 상어가 이동가능한 물고기 리스트들을 만든다.
-def fishlist(i, j, dir):
-    dy,dx = direction[dir]
+# 상어가 먹을 수 있는 물고기 리스트
+def fishlist(i, j, dir, graph):
+    dy, dx = direction[dir]
     queue = []
-    for k in range(1, 4):
-        ny, nx = dy * k + i, dx * k + j
-        if N > ny >= 0 and N > nx >= 0 and graph[ny][nx] != 0: #물고기라면
+    for k in range(1, N):
+        ny = (dy * k) + i
+        nx = (dx * k) + j
+        if N > ny >= 0 and N > nx >= 0 and graph[ny][nx][0] != EMPTY: #물고기라면
             queue.append((ny, nx)) # 먹을 수 있는 후보를 탐색한다.
 
     return queue
 
+def findfish(fishnum, graph):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j][0] == fishnum:
+                return (i, j)
+def movefish(i, j, graph):
+    visited = [False for _ in range(len(direction))]
+    dir = graph[i][j][1]
 
-def movefish():
-    newqueue = []
+    while True: # 움직일수있는 경우까지 찾는다.
+        visited[dir] = True
+        dy, dx = direction[dir]
+        ny, nx = dy + i, dx + j
+        #빈칸과 물고기가 있는칸은 이동 할 수 있다.
+        if N > ny >=0 and N > nx >=0 and SHARK != graph[ny][nx][0]: # 상어가 아닌칸과, 범위를 벗어나는 경우
+            temp1, temp2 = graph[ny][nx][0], graph[ny][nx][1] # 물고기 번호, 방향
+            # 물고기 자리를 바꾼다.
+            graph[ny][nx][0] = graph[i][j][0]
+            graph[ny][nx][1] = dir
 
-    fishqueue.sort()
-    for num, dir, i, j in fishqueue:
+            graph[i][j][0] = temp1
+            graph[i][j][1] = temp2
+            break
 
-        visited = [False for _ in range(9)]
-        visited[0] = True
+        if all(visited):
+            # 45도씩 회전하면서 다 돌았는데 갈 수 없는경우
+            break
 
-        while True: # 움직일수있는 경우까지 찾는다.
-            visited[dir] = True
-            dy, dx = direction[dir]
-            ny, nx = dy +i, dx + j
-            #빈칸과 물고기가 있는칸은 이동 할 수 있다.
-            if N > ny >=0 and N > nx >=0 and 20 > graph[ny][nx][0]: # 상어는 20으로 표현하겠음.
-                # 탐색할 수 있는경우 물고기의 위치를 바꾼다.
-                temp1, temp2 = graph[ny][nx][0], graph[ny][nx][1] # 물고기 번호, 방향
+        dir = (dir + 1) % len(direction)
 
-                # 물고기 자리를 바꾼다.
-                graph[ny][nx][0] = num
-                graph[ny][nx][1] = dir
 
-                graph[i][j][0] = temp1
-                graph[i][j][1] = temp2
+def dfs(i, j, graph, total):
+    global ans
+    graph = deepcopy(graph)
+    # 상어가 물고기를 먹는다.
+    graph[i][j][0] = SHARK
+    shark_direction = graph[i][j][1]
 
-                fishqueue[num - 1] = [graph[i][j][0], graph[i][j][1], i, j]
-                break
-            elif all(visited):
-                # 45도씩 회전하면서 다 돌았는데 갈 수 없는경우
-                break
-            else:
-                dir = rotate(dir) # 방향을 회전시킨다.
+    # 물고기가 움직인다.
+    for n in range(1, 17): # 1번 물고기부터 16번 물고기 까지 차례대로 움직인다.
+        fish = findfish(n, graph)
+        if fish:
+            ny_fish, nx_fish = fish[0], fish[1]
+            movefish(ny_fish, nx_fish, graph)
 
-    return newqueue # 새로운 물고기 큐 리턴한다.
-
+    # 상어가 먹을수 있는 물고기 리스트들을 구한다.
+    graph[i][j][0] = EMPTY
+    fish_list = fishlist(i, j, shark_direction, graph)
+    #pprint(fish_list)
+    #pprint(graph)
+    if fish_list:
+        for ny_shark, nx_shark in fish_list:
+            dfs(ny_shark, nx_shark, graph, total + graph[ny_shark][nx_shark][0])
+    else:
+        ans = max(ans, total)
+        return
 
 ans = 0
-def dfs(queue, visitedq):
-    global visitedqueue
-    if not queue:
-        visitedqueue = deepcopy(visitedq)
-        return visitedq
+dfs(0, 0, graph, graph[0][0][0])
 
-    # 물고기를 먹는다.
-    result = 0
-    i, j = queue.pop()
-    fishnum = graph[i][j][0]
-    newdir = graph[i][j][1]
-    graph[i][j][0] = 0 # 물고기를 먹는다.
-    newqueue = fishlist(i, j, newdir)
-    visitedq.append((i,j))
-    dfs(newqueue, visitedq)
-    visitedq.pop()
-    queue.append((i, j))
-    graph[i][j][0] = fishnum
-
-
-ans = 0
-while True:
-    c = canmoveshark()
-    if c is None: # 상어가 더이상 움직일 수 없다면 끝난다.
-        break
-
-    fishqueue = movefish() # 물고기가 움직인다.
-    maxeat = 0
-    pprint(graph)
-    queue = fishlist(0, 0, c) # 상어가 먹을 수 있는 물고기 후보들
-    visitedqueue = []
-
-    dfs(queue, []) # 다 먹어버린다.
-    for i, j in visitedqueue:
-        ans += graph[i][j][1]
-        graph[i][j][0] = 0
-
-    graph[0][0][0] = 0 # 상어가 다시 집으로간다.
-
-    fishqueue = movefish() # 물고기가 움직인다.
+print(ans)
