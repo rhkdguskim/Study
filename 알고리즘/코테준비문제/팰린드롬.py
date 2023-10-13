8d2ed39a9c546d313a5dac3cc75144bac8ecc83f
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-10-13 19:47:55 +0900

    알고리즘 스터디

--- 알고리즘/코테준비문제/팰린드롬.py
+++ 알고리즘/코테준비문제/팰린드롬.py
@@ -1,42 +1,29 @@
 # https://www.acmicpc.net/problem/10942
 import sys
-sys.setrecursionlimit(20000000)
+input = sys.stdin.readline
 
 N = int(input())
-arr = list(map(int ,input().split()))
+arr = list(map(int, input().split()))
 M = int(input())
-
 
 dp = [[0 for _ in range(N)] for _ in range(N)]
 
-def dfs(start, end):
-    if 0 > start and end > N:
-        return 0
+for i in range(N): # 길이가 1인 펠린드롬
+    dp[i][i] = 1
 
-    if start > end:
-        return 0
+for i in range(N - 1): # 길이가 2인 펠린드롬
+    if arr[i] == arr[i + 1]:
+        dp[i][i + 1] = 1
     else:
-        left = start
-        right = end
-        if left == right: # 자기자신은 팰린드롬이다
-            dp[left][right] = 1
-            return 1
+        dp[i][i + 1] = 0
+
+for i in range(2, N): # 길이가 3이상인 펠린드롬 ( N펠린드롬을 구할때 N-1 펠린드롬을 참고하여 구한다.)
+    for j in range(N - i):
+        if arr[j] == arr[j+i]:
+            dp[j][j+i] = dp[j+1][j+i-1]
         else:
-            if arr[left] == arr[right]: # 만약 팰린드롬이라면
-                dp[left][right] = dfs(start +1, end -1) # +1, -1 재귀로 했을때
-            else:
-                dp[left][right] = 0
-                
-            dp[left][right-1] = dfs(start, end-1)
-            dp[left+1][right] = dfs(start+1, end)
-            
-    return dp[left][right]
-    
-dfs(0, N-1)
-    
+            dp[j][j+i] = 0
+
 for _ in range(M):
     left, right = map(int, input().split())
-    print(dp[left-1][right-1])
-    
-
-    
+    print(dp[left - 1][right - 1])
