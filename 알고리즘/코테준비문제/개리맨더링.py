e24988c6b3f20c7a1e08a242a51e6cae21e2344d
rhkdguskim@naver.com <rhkdguskim@naver.com>
2023-10-16 23:15:27 +0900

    알고리즘 스터디

--- 알고리즘/코테준비문제/개리맨더링.py
+++ 알고리즘/코테준비문제/개리맨더링.py
@@ -3,6 +3,7 @@
 # 2. A구역, B구역으로 각각 잘 나누어 졌는지 확인해야한다.
 # 3. 확인하는 방법은 A구역을 확인한다고하면 A구역의 아무 점이나 선택하여 깊이우선탐색을 하였을때, 방문길이가 반드시 구역의 크기 만큼 있어야한다.
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
@@ -15,23 +16,28 @@ for i in range(1, N+1):
             graph[i].append(n)
 
 
-def dfs(n, team, visited):
-    if len(team) == len(visited):
-        return True
-    ret = False
-    for n_n in graph[n]:
-        if n_n in team and n_n not in visited:
-            visited.append(n_n)
-            ret |= dfs(n_n, team, visited)
-
-    return ret
-
 def is_team(team):
-    n = team[0]
-    return dfs(n, team, [n])
+    queue = deque()
+    queue.append(team[0])
+    visit = [False for _ in range(N+1)]
+    for k in range(N+1):
+        if k not in team:
+            visit[k] = True
+    visit[team[0]] = True
+
+    while queue:
+        new_n = queue.popleft()
+        for new_n2 in graph[new_n]:
+            if new_n2 in team and not visit[new_n2]:
+                visit[new_n2] = True
+                queue.append(new_n2)
+
+    if all(visit):
+        return True
+    else:
+        return False
 
 ans = sum(people)
-ret = False
 
 def people_diff(team, team2):
     temp = 0
@@ -43,25 +49,26 @@ def people_diff(team, team2):
 
     return abs(temp - temp2)
 
-def make_team(team):
+def make_team(idx, team):
     global ans
+    if team and team[0] > N // 2:
+        return
 
-    if len(team) > 0: # 모든 팀이 결성이 되면
+    if N > len(team) > 0: # 모든 팀이 결성이 되면
         team2 = [i for i in range(1, N+1) if i not in team]
-        print(team, team2)
         if is_team(team) and is_team(team2):
-            print(team, team2)
+            #print(team, team2)
             ans = min(people_diff(team, team2), ans)
-            print(ans)
+            #print(ans)
 
-    for n in range(1, N + 1):
+    for n in range(idx, N + 1):
         if n not in team:
             team.append(n)
-            make_team(team)
+            make_team(idx + 1,team)
             team.pop()
 
 
-make_team([])
+make_team(1, [])
 if ans == sum(people):
     print(-1)
 else:
