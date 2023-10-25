1a57fbe12e2dfe8714de9d0cb8ad078cf9d93ee8
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-10-25 23:11:55 +0900

    분할정복

--- 알고리즘/새로운시작/Moo 게임.py
+++ 알고리즘/새로운시작/Moo 게임.py
@@ -1,16 +1,42 @@
 # https://www.acmicpc.net/problem/5904
 
-def dfs(N, cur, depth):
-    if 3 >= N:
-        print(N)
+# 왼 + 중간값 + 우
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+total_len = 3
+mid = 0
+while total_len < N:
+    mid += 1
+    total_len = total_len*2 + (3 + mid) # 중간값, 우를 더한다.
+    
+
+def moo(total_len, mid_len, N):
+    if N <= 3:
         if N == 1:
             print("m")
         else:
-            print("o")
+            print('o')
         return
-    cost = cur*3 + 3 + depth
-    N -= cost   
-    dfs(N, cost, depth+1) 
     
-dfs(11, 0, 0)
+    left_len = (total_len - mid_len) // 2
+    
+    if N <= left_len: #왼쪽으로 재귀
+        return moo(left_len, mid_len-1, N)
+    
+    if N > left_len + mid_len: # 오른쪽에 있는경우
+        return moo(left_len, mid_len-1, N - (left_len + mid_len))
+    
+    
+    if N - left_len == 1:
+        print('m')
+    else:
+        print('o')
+    
+
+
+moo(total_len, mid + 3, N)
+    
+
     
