b0a4762b6a7f8cdfd24e0a5cdc5ace698aff21ea
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-10-29 23:19:17 +0900

    알고리즘 스터디

--- 알고리즘/새로운시작/별 찍기 - 10.py
+++ 알고리즘/새로운시작/별 찍기 - 10.py
@@ -1,23 +1,24 @@
 # https://www.acmicpc.net/problem/2447
-from copy import deepcopy
+import sys
+input = sys.stdin.readline
+
 k = int(input())
 
-n = 1
-star = ['' for _ in range(3)]
-while n > k:
-    newstar = ['' for _ in range(3)]
-    if n == 1: # 초기
-        newstar[0] = '*'*3
-        newstar[1] = '*' + ' ' + '*'
-        newstar[2] = '*'*3
-    else: # 이전에 있던 star 값을 사용
-        for n in range(3):
-            for s in star[n]:
-                if n == 1:
-                    pass
-                else:
-                    newstar[n] += s
-                    newstar[n] += '\n'
+def dfs(n):
+    if n == 1:
+        return ['*']
+    
+    star = dfs(n//3)
+    result = []
+    for s in star:
+        result.append(s*3)
+    
+    for s in star:
+        result.append(s + ' '*(n//3) + s)
         
-    star = deepcopy(newstar)
-    n *= 3
+    for s in star:
+        result.append(s*3)
+        
+    return result
+
+print('\n'.join(dfs(k)))
