03ec6cd62d1c850a67aad2aec2b06a08d47cec8b
김광현 <rhkdguskim@naver.com>
2024-09-04 13:01:09 +0900

    feat : 점심시간 알고리즘 문제풀이

--- Algorithm/C++/프로그래머스/혼자 놀기의 달인.cpp
+++ Algorithm/C++/프로그래머스/혼자 놀기의 달인.cpp
@@ -12,61 +12,66 @@ using namespace std;
 // 3. 가르키는 상자가 열려있다면 종료 -> 1번으로 이동
     // box를 추가한다. 여기서 추가된 개수를 계산한다.
 // 4. 가르키는 상자가 닫혀있다면 연다 -> 2번으로 이동
-int dfs(vector<int> &cards, vector<bool> &v, vector<int> &boxs, int cur_card, int cnt);
+int dfs(vector<int>& cards, vector<bool>& v, vector<int>& boxs, int cur_card, int cnt, vector<vector<int>>& dp);
 
 int solution(vector<int> cards) {
     int answer = 0;
     vector<int> boxs;
+    vector<vector<int>> dp;
+    dp.resize(cards.size(), vector<int>(cards.size(), -1));
     vector<bool> v(cards.size(), false);
-    for(int i = 0; i < cards.size(); i++) {
+    for (int i = 0; i < cards.size(); i++) {
         boxs.push_back(1);
-        auto value = dfs(cards, v, boxs, i, 1);
+        v[i] = true;
+        answer = max(answer, dfs(cards, v, boxs, i, 1, dp));
+        v[i] = false;
         boxs.pop_back();
-        answer = max(value, answer);
     }
     return answer;
 }
 
-int dfs(vector<int> &cards, vector<bool> &v, vector<int> &boxs, int cur_card, int cnt)
+int dfs(vector<int>& cards, vector<bool>& v, vector<int>& boxs, int cur_card, int cnt, vector<vector<int>> &dp)
 {
     int card_size = cards.size();
     // 상자가 모두 열려있는경우
-    if(cnt == card_size) {
-        // 박스가 하나 이하인경우 0
-        if(1 >= boxs.size()) return 0;
+    if (cnt == card_size) {
+        // 박스가 하나인경우
+        if (1 == boxs.size()) return 0;
 
-        // 그렇지 않은경우 상자를 모두 곱한다.
+        vector<int> temp = boxs;
+        sort(temp.begin(), temp.end());
+
         int ans = 1;
-        for(auto box : boxs) {
-            ans *= box;
+        for (int i = 0; i < 2; i++) {
+            ans *= temp.back();  temp.pop_back();
         }
 
         return ans;
-
     }
 
-    int value = 0;
+    if (dp[cur_card][cnt - 1] != -1) return dp[cur_card][cnt - 1];
+
     int max_v = 0;
-    if(v[cur_card]) {
-        for(int i = 0; i < card_size; i ++) {
-            if(v[i]) continue;
+    auto next = cards[cur_card] - 1;
+    if (v[next]) {
+        for (int i = 0; i < card_size; i++) {
+            if (v[i]) continue;
 
             v[i] = true;
             boxs.push_back(1);
-            value = dfs(cards, v, boxs, i, cnt + 1);
-            max_v = max(max_v, value);
+            dp[cur_card][cnt-1] = dfs(cards, v, boxs, i, cnt + 1, dp);
+            max_v = max(max_v, dp[cur_card][cnt - 1]);
             boxs.pop_back();
             v[i] = false;
         }
-    } else {
-        int box_len = boxs.size();
-        
-        boxs[box_len - 1] += 1;
-        v[cur_card] = true;
-        value = dfs(cards, v, boxs, cards[cur_card], cnt + 1);
-        max_v = max(max_v, value);
-        v[cur_card] = false;
-        boxs[box_len - 1] -= 1;
+    }
+    else {
+        boxs.back() += 1;
+        v[next] = true;
+        dp[cur_card][cnt - 1] = dfs(cards, v, boxs, next, cnt + 1, dp);
+        max_v = max(max_v, dp[cur_card][cnt - 1]);
+        v[next] = false;
+        boxs.back() -= 1;
     }
 
     return max_v;
