11c69aed84481d8bed53c7a32ef1fe61ec244e51
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2024-11-27 18:45:45 +0900

    [Gold V] Title: 물병, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -1,25 +1,11 @@
-import sys, heapq
+import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-start = 1
-q = []
-while N > 1:
-    if N % 2 != 0:
-        heapq.heappush(q, start)
-    N //= 2
-    start *= 2
+cnt = 0
+while bin(N)[2:].count('1') > K: 
+    cnt += N&-N
+    N += N&-N
     
-heapq.heappush(q, start)
-
-cost = 0
-while len(q) > K:
-    v = heapq.heappop(q)
-    if q[0] == v:
-        heapq.heappop(q)
-        heapq.heappush(q, v*2)
-    else:
-        cost += v
-        heapq.heappush(q, v*2)
-print(cost)
+print(cnt)
