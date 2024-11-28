539931d19a5395cf60f3c267548dc280f672fb51
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2024-11-28 19:30:37 +0900

    [Gold IV] Title: 세 수의 합, Time: 152 ms, Memory: 65940 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,38 +1,14 @@
 import sys
+from itertools import combinations_with_replacement as cwr
+
 input = sys.stdin.readline
 
 N = int(input())
-arr = [int(input()) for _ in range(N)]
+arr = sorted([int(input()) for _ in range(N)])[::-1]
+arr_sum = {x+y for x, y in cwr(arr, 2)}
 
-arr_sum = []
-arr_sub = []
-for a in arr:
-    for b in arr:
-        arr_sum.append(a+b)
-        if a >= b:
-            arr_sub.append([a-b, a])
-            
-def find(arr, v):
-    start, end = 0, len(arr)-1
-    idx = -1
-    while end >= start:
-        mid = (start + end) // 2
-        if arr[mid][0] >= v:
-            idx = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    return idx
-
-arr_sum.sort(reverse=True)
-arr_sub.sort()
-
-ans = []
-for v in arr_sum:
-    idx = find(arr_sub, v)
-    if idx != -1 and idx != len(arr_sub):
-        if v == arr_sub[idx][0]:
-            ans.append(arr_sub[idx][1])
-            
-ans.sort()
-print(ans[-1])
+for i in arr:
+    for j in arr:
+        if i >= j and (i-j) in arr_sum:
+            print(i)
+            exit()
